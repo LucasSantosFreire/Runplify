92536194b1ad5321031793475abb02b9963b7dba
Lucas Santos <lucas.santos@acensjr.com>
2023-06-10 14:34:02 -0300

    added command in pallete

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,54 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the runplify extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'runplify:plugin',
+  id: 'runplify',
   description: 'A Juupyter lab extension to save execution output',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension runplify is activated!');
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension Runplify is activated!');
+
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'runplify';
+      widget.title.label = 'Runplify';
+      widget.title.closable = true;
+      return widget;
+    }
+    let widget = newWidget();
+
+    // Add an application command
+    const command: string = 'runplify:open';
+    app.commands.addCommand(command, {
+      label: 'Runplify',
+      execute: () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
